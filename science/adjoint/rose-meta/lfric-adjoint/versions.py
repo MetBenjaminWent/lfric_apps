c5cf04665fbabc915eb36360bfde84ac26be217c
MetBenjaminWent <benjamin.went@metoffice.gov.uk>
2026-03-18 15:55:20 +0000

    Add new versions for other apps rose meta

--- science/adjoint/rose-meta/lfric-adjoint/versions.py
+++ science/adjoint/rose-meta/lfric-adjoint/versions.py
@@ -33,11 +33,11 @@ class vnXX_txxx(MacroUpgrade):
 """
 
 
-class vn31_t(MacroUpgrade):
-    """Upgrade macro for ticket #221 by Benjamin Went."""
+class vn31_t382(MacroUpgrade):
+    """Upgrade macro for ticket #382 by Benjamin Went."""
 
     BEFORE_TAG = "vn3.1"
-    AFTER_TAG = "vn3.1_t"
+    AFTER_TAG = "vn3.1_t382"
 
     def upgrade(self, config, meta_config=None):
         # Commands From: rose-meta/lfric-lfric_atm
